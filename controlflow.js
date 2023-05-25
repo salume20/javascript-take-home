0f6fa5acca8e9d70021cf08ffb6f019948ff00dd
ADONG OLIVER SALUME <salumea20@gmail.com>
2023-05-25 12:52:19 +0300

    Controlflow

---
+++ controlflow.js
@@ -0,0 +1,41 @@
+//for loops
+//for (let i = 0; i < 5; i++){
+  //console.log("in loop", i);  
+//}
+//console.log("loop finished");
+
+//for(let i =1; i< 20; i++){
+  //console.log("in loop:", i);
+//}
+//console.log("loop done")
+//const names =["Richard","Patrick", "Andrew", "David"];
+//for(let i = 0; i<names.length; i++){
+  //console.log(names[i]);
+
+//
+//}
+//while loop
+//let i=0
+//while ( i < 10 ){
+  //console.log("in loop", i);
+  //i++
+//}
+//do while loop
+//let i =1;
+//do{
+  //console.log("value of i is: ", i);
+  //i++;
+//}
+//while(i <10)
+//if statements
+//const age = 40;
+
+//if (age > 20){
+  //console.log("You are above 20 years old");
+
+//}
+const password = 12345678900;
+
+if(password.length >= 10){
+  console.log("Great You are good to go");
+}
